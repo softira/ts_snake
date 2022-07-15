d1687b7a8d6166c387e17fda356dc4ca974ce72c
softira <1160390705@qq.com>
2022-07-15 08:06:06 +0800

    创建对象

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,14 @@
 // 引入样式
 import './style/index.less'
 
-// 定义一个食物类
-class Food {
-  element: HTMLElement
-  constructor(){
-    this.element = document.getElementById('food')!
-  }
-  get x(){
-    return this.element.offsetLeft
-  }
-  get y(){
-    return this.element.offsetHeight
-  }
-  change(){
-    let x = Math.round(Math.random()*29)*10
-    let y = Math.round(Math.random()*29)*10
-    this.element.style.left = x + 'px'
-    this.element.style.top = y + 'px'
-  }
-}
-let food = new Food()
-food.change()
+import Food from './moduls/Food'
+import Scorecard from './moduls/Scorecard'
+
+
+// 测试代码
+// let food = new Food()
+// food.change()
+// let score = new Scorecard()
+// for(let i = 0;i < 200;i++){
+//   score.scoreIncrease()
+// }
---
+++ src/moduls/Food.ts
@@ -0,0 +1,23 @@
+// 定义一个食物类
+class Food {
+  element: HTMLElement
+  constructor(){
+    this.element = document.getElementById('food')!
+  }
+  // 获取事物位置
+  get x(){
+    return this.element.offsetLeft
+  }
+  get y(){
+    return this.element.offsetHeight
+  }
+  // 改变事物位置
+  change(){
+    let x = Math.round(Math.random()*29)*10
+    let y = Math.round(Math.random()*29)*10
+    this.element.style.left = x + 'px'
+    this.element.style.top = y + 'px'
+  }
+}
+
+export default Food
